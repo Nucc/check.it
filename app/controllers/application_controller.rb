258519731f2f183efbb3fa6bc838c1e6c8df9782
Nucc <nucc@bteam.hu>
2011-07-28 00:31:37 +0200

    Adding title to subpages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_title
+  
+  def set_title
+    @page_title = title
+  end
+  
+  def title
+    @page_title || ""
+  end
 end
--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -2,10 +2,6 @@ class PatchesController < ApplicationController
 
   before_filter :authenticate_user!
 
-  def repository
-    @repository ||= Repository.new("/Users/developer/Desktop/#{params[:repository_id]}", params[:branch_id].to_s)
-  end
-
   def index
     @pager = ::Paginator.new(repository.count, 30) do |offset, per_page|
       repository.patches(per_page, offset)
@@ -23,5 +19,17 @@ class PatchesController < ApplicationController
       @comments = []
     end
   end
+
+private
+
+  def title
+    "#{repository.name} [#{repository.branch}]"
+  end
+
+  def repository
+    @repository ||= Repository.new("/Users/developer/Desktop/#{params[:repository_id]}", params[:branch_id].to_s)
+  end
+
+
   
 end
