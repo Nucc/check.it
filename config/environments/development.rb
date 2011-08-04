8e72a880e3269bcaedead7f9d0e9da430691d8fe
Nucc <nucc@bteam.hu>
2011-08-05 00:21:07 +0200

    Development log level is info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ Reviewer::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.log_level = :info
 end
 
