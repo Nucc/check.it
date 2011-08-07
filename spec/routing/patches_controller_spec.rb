3637969b9960e00c91b12323e82234e360abc060
Nucc <nucc@bteam.hu>
2011-08-08 00:13:08 +0200

    Routing spec of PatchesController moved to the routing directory

---
+++ spec/routing/patches_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe PatchesController do
+  
+  it "/patches => /repositories"
+  
+  it "/repositories/../patches" do
+    { :get => "/repositories/test.git/patches" }.
+      should route_to(:controller => "patches",
+                      :action => "index",
+                      :repository_id => "test.git")
+  end
+  
+  it "/repositories/../branches/../patches" do
+    { :get => "/repositories/test.git/branches/test.branch/patches" }.
+      should route_to(:controller => "patches",
+                      :action => "index",
+                      :repository_id => "test.git",
+                      :branch_id => "test.branch")
+  end
+  
+end
+ 
