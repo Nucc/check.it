207fbf67aaa3013bd1dcdb6a45b119553a9715e1
Nucc <nucc@bteam.hu>
2011-12-22 13:38:51 +0100

    Fixing the database migration problem
     * the name of the migration file differed from the class name in it

--- db/migrate/20110514124958_create_comments.rb
+++ db/migrate/20110514124958_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.commit         :commit_id, :null => false
+      t.integer         :commit_id, :null => false
       t.string         :opinion, :null => false
       t.integer        :block, :null => false
       t.integer        :line, :null => false
