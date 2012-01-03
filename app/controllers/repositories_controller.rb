7ac84df34155c3effb9ad5ab8099c18c841a6089
Nucc <nucc@bteam.hu>
2012-01-03 18:24:05 +0100

    Don't show "." and ".." directories in repository list

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -12,6 +12,9 @@ protected
     repositories    = []
     repository_path = CONFIG["repository_path"]
     Dir.foreach(repository_path).each do |dir|
+      
+      next if dir == ".." or dir == "."
+      
       begin
         repositories << Repository.new("#{repository_path}/#{dir}")
       rescue Grit::InvalidGitRepositoryError
