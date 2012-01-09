df8388952112982cefc69cb0ca202eb0c94704b2
Nucc <nucc@bteam.hu>
2012-01-09 22:38:15 +0100

    Use Repository.all instead of repository method in repositories_controller

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,26 +5,11 @@ class RepositoriesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @repositories = repositories
+    @repositories = Repository.all
   end
 
 protected
   
-  def repositories
-    repositories    = []
-    repository_path = CONFIG["repository_path"]
-    Dir.foreach(repository_path).each do |dir|
-      
-      next if dir == ".." or dir == "."
-      
-      begin
-        repositories << Repository.new("#{repository_path}/#{dir}")
-      rescue Grit::InvalidGitRepositoryError => e
-      end
-    end
-    return repositories
-  end
-
   def title
     "Repositories and branches"
   end
