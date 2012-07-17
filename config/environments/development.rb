bb785f356e5bc41f1d0631f457e7e93a66db413f
Nucc <nucc@bteam.hu>
2012-07-17 15:55:23 +0200

    Use debug log level in development env by default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,6 @@ Reviewer::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   
-  config.log_level = :info
+  config.log_level = :debug
 end
 
