ef2dac3900c52ca014fb7c4b20920b778312d273
Nucc <nucc@bteam.hu>
2012-07-18 11:28:25 +0200

    Show time in comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,15 +5,14 @@ module ApplicationHelper
   end
 
   def format_date(date)
-
     return date.to_s
-
-    if date > Time.now - 7.days
-      time_ago_in_words(date) + " ago"
-    else
-      date.strftime("%b %d, %Y")
-    end
   end
+
+  def human_time(date)
+    date.strftime("%Y %b %d, %H:%M")
+  end
+
+
 
   def format_patch(patch)
     link_to(patch, repository_patch_url(patch.repository, patch))
